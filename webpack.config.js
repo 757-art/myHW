8446cfec64373b69ea6c220c759add91f05bff0f
Artem Stepanyuk <aivanov757@gmail.com>
2022-09-23 10:24:18 +0300

    Готовый вариант

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExstractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExstractPlugin.loader,
+                    'css-loader'
+            ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ 
+            template: './src/index.html',
+            filename: 'main.html'
+        }),
+        new MiniCSSExstractPlugin()
+    ]
+
+}
